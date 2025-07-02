cef4c5cd4b4af77a7044d8fc13b8300b5894c6f3
michelangelomancini4 <michelman93@tiscali.it>
2025-07-02 09:53:02 +0200

    Milestone 3 completed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 type Person = {
   readonly id: number,
-  readonly name: [string, string],
+  readonly name: string,
   birth_year: number,
   death_year?: number,
   biography: string,
@@ -14,3 +14,49 @@ type Actress = Person & {
   awards: string,
   nationality: ActressNationality
 }
+
+
+const url = `http://localhost:3333/actresses/`;
+
+
+function isActress(dati: unknown): dati is Actress {
+
+  return (
+    typeof dati === 'object' && !dati == null &&
+    "id" in dati && typeof dati.id === 'number' &&
+    "name" in dati && typeof dati.name === 'string' &&
+    "birth_year" in dati && typeof dati.birth_year === 'number' &&
+    "death_year" in dati && typeof dati.death_year === 'number' &&
+    "biography" in dati && typeof dati.biography === 'string' &&
+    "image" in dati && typeof dati.image === 'string' &&
+    "most_famous_movies" in dati && dati.most_famous_movies instanceof Array && dati.most_famous_movies.length === 3 && dati.most_famous_movies.every(m => typeof m === 'string') &&
+    "awards" in dati && typeof dati.awards === 'string' &&
+    "nationality" in dati && typeof dati.nationality === 'string'
+  )
+}
+
+// function milestone 3
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`${url}${id}`)
+    const dati: unknown = await response.json()
+    if (!isActress(dati)) {
+      throw new Error("Formato dei dati non valido");
+
+    }
+    return dati;
+
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Errore durante recupero dati attrice', error)
+    } else {
+      console.error('Errore sconosciuto', error);
+
+    }
+    return null;
+  }
+}
+
+
+
